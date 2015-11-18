4faa104e7a9064cac6e3cf993cc6a942abfac869
Jolan Levy <jolan.levy@gmail.com>
2015-11-18 10:38:56 +0100

    Essai de push sur GitHub

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
 | First we need to get an application instance. This creates an instance
 | of the application / container and bootstraps the application so it
 | is ready to receive HTTP / Console requests from the environment.
-|
+| Try
 */
 
 $app = require __DIR__.'/../bootstrap/app.php';
