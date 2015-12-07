6595ca0b51acaa41197c576a11b08feba3bd17bc
Antoine Cusset <a.cusset@gmail.com>
2015-12-08 00:38:54 +0100

    ajout seeds

--- database/seeds/AnswerTableSeeder.php
+++ database/seeds/AnswerTableSeeder.php
@@ -18,28 +18,28 @@ class AnswerTableSeeder extends Seeder
          * Question n°1
          */
         DB::table('answers')->insert([
-            'question_id' => 1,
+            'question_id' => intval(DB::table('questions')->all()->first()->id),
             'label' => 'La Thaîlande',
             'correct' => true,
             'created_at' => $date,
             'updated_at' => $date
         ]);
         DB::table('answers')->insert([
-            'question_id' => 1,
+            'question_id' => intval(DB::table('questions')->all()->first()->id),
             'label' => 'La Cambodge',
             'correct' => false,
             'created_at' => $date,
             'updated_at' => $date
         ]);
         DB::table('answers')->insert([
-            'question_id' => 1,
+            'question_id' => intval(DB::table('questions')->all()->first()->id),
             'label' => 'La Malaisie',
             'correct' => false,
             'created_at' => $date,
             'updated_at' => $date
         ]);
         DB::table('answers')->insert([
-            'question_id' => 1,
+            'question_id' => intval(DB::table('questions')->all()->first()->id),
             'label' => 'L\'indonésie',
             'correct' => false,
             'created_at' => $date,
@@ -50,28 +50,28 @@ class AnswerTableSeeder extends Seeder
          * Question n°2
          */
         DB::table('answers')->insert([
-            'question_id' => 2,
+            'question_id' => intval(DB::table('questions')->all()->first()->id + 1),
             'label' => 'Qu\'il n\'est pas arrivé à Toronto',
             'correct' => true,
             'created_at' => $date,
             'updated_at' => $date
         ]);
         DB::table('answers')->insert([
-            'question_id' => 2,
+            'question_id' => intval(DB::table('questions')->all()->first()->id + 1),
             'label' => 'Qu\'il était supposé arriver à Toronto ...',
             'correct' => false,
             'created_at' => $date,
             'updated_at' => $date
         ]);
         DB::table('answers')->insert([
-            'question_id' => 2,
+            'question_id' => intval(DB::table('questions')->all()->first()->id + 1),
             'label' => '"Qu\'est-ce qu\'il fout ce maudit pancake, tabernacle ?"',
             'correct' => false,
             'created_at' => $date,
             'updated_at' => $date
         ]);
         DB::table('answers')->insert([
-            'question_id' => 2,
+            'question_id' => intval(DB::table('questions')->all()->first()->id + 1),
             'label' => 'La réponse D',
             'correct' => false,
             'created_at' => $date,
