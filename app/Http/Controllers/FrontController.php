cd400554c47603a19a9ecca2469d3a54a37f6876
hpiso <hpiso@pm-00037.local>
2016-02-11 01:06:37 +0100

    Some fix

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,9 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Answer;
-use App\Models\Question;
 use Illuminate\Http\Request;
-use App\Models\Quizz;
 use App\Models\Score;
 use App\Repositories\QuizzRepository;
 use App\Repositories\QuestionRepository;
