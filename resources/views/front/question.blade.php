3ec923c5b9589fe9a496d847cddfd7c264fc1f53
Jolan Levy <jolan.levy@gmail.com>
2016-02-19 17:30:03 +0100

    New design du front

--- resources/views/front/question.blade.php
+++ resources/views/front/question.blade.php
@@ -1,46 +1,61 @@
 @extends('layout.front')
 
 @section('content')
+    <div id="address-bar" class="address-bar"><?php echo 'Question '.($numQuest+1).' sur '.$nbQuest; ?></div>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        @if($numQuest+1==$nbQuest )
+                            <a id="nav_btn"  class="hvr-shutter-out-vertical">VALIDER</a>
+                        @else
+                            <a id="nav_btn" class="hvr-shutter-out-vertical">SUIVANT</a>
+                        @endif
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <div class="row">
-        <div class="col s12">
-            <br><br><br>
-            <div id="compteRebour"></div>
-            <form method="post" action="{{ route('quizz.action') }}">
-                <h5>{{$question->label}}</h5>
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="row liste_questions">
-                    <br>
-                    <?php $random=array(); ?>
-                    @foreach($question->answers as $rep)
-                        <?php array_push($random,$rep) ?>
-                    @endforeach
-                    <?php shuffle($random); ?>
-                    @foreach($random as $rep )
-                        <div class="col s6">
-                            <p>
-                                <input name="answer" value="{{$rep->id}}" type="radio" id="{{$rep->id}}" />
-                                <label for="{{$rep->id}}">{{$rep->label}}</label>
-                            </p>
-                        </div>
-                    @endforeach
-                </div>
-                @if($numQuest+1==$nbQuest)
-                    <label for="subForm">
-                        <input id="subForm" class="input btn btn-large waves-effect waves-light blue" type="submit" value="Valider">
-                    </label>
-                @else
-                    <label for="butNext">
-                        <input id="butNext" class="input btn-floating btn-large waves-effect waves-light blue" type="submit" value=">>">
-                    </label>
-                @endif
-            </form>
+        <div class="box">
+            <div class="col-lg-12">
+                <form method="post" id="quest-form" action="{{ route('quizz.action') }}">
+                    <hr>
+                    <h2 class="intro-text text-center"><strong>{{$question->label}}</strong></h2>
+                    <hr>
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div id="compteRebour"></div>
+                    <div class="modal-body">
+                        <div class="quiz col-lg-8 col-lg-offset-2" id="quiz" data-toggle="buttons">
+                            <?php $random=array(); ?>
+                            @foreach($question->answers as $rep)
+                                <?php array_push($random,$rep) ?>
+                            @endforeach
+                            <?php shuffle($random); ?>
+                            @foreach($random as $rep )
+                                    <label class="element-animation1 btn btn-lg btn-primary btn-block">
+                                        <span class="btn-label">
+                                            <i class="glyphicon glyphicon-chevron-right"></i>
+                                        </span>
+                                        <input type="radio" name="answer" value="{{$rep->id}}">
+                                        {{$rep->label}}
+                                    </label>
+                            @endforeach
+                         </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
     <script type="text/javascript">
         clearTimeout(timeout);
-        rebour(15, {{$numQuest+1}}, {{$nbQuest}});
-        var text = '<?php echo 'Question '.($numQuest+1).' sur '.$nbQuest; ?>';
-        $('.nav-wrapper .nav-link').html(text);
-        $('.nav-wrapper .brand-logo').removeAttr('href');
+        {{--rebour(15, {{$numQuest+1}}, {{$nbQuest}});--}}
+        document.getElementById("nav_btn").onclick = function() {
+            document.getElementById("quest-form").submit();
+        };
     </script>
 @endsection
