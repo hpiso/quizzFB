e7d08017e7d971659da00f6b1030956fae2ee4c5
Antoine Cusset <a.cusset@gmail.com>
2016-02-28 22:00:13 +0100

    remplacement des http par des https pour tester

--- resources/views/admin/common/global-css.blade.php
+++ resources/views/admin/common/global-css.blade.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 <link href="{{ url('/css/metisMenu.min.css') }}" rel="stylesheet" type="text/css" >
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.10/css/dataTables.bootstrap.min.css">
-<link href="{{ url('/css/font-awesome-4.4.0/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" >
-<link href="{{ url('/css/sb-admin-2.css') }}" rel="stylesheet" type="text/css" >
-<link href="{{ url('/css/bootstrap-datepicker.min.css') }}" rel="stylesheet" type="text/css" >
-<link href="{{ url('/css/bootstrap-select.min.css') }}" rel="stylesheet" type="text/css" >
-<link href="{{ url('/css/bootstrap-colorpicker.min.css') }}" rel="stylesheet" type="text/css" >
-<link href="{{ url('/css/style.css') }}" rel="stylesheet" type="text/css" >
+<link href="{{ url('/css/font-awesome-4.4.0/css/font-awesome.min.css',[],true) }}" rel="stylesheet" type="text/css" >
+<link href="{{ url('/css/sb-admin-2.css',[],true) }}" rel="stylesheet" type="text/css" >
+<link href="{{ url('/css/bootstrap-datepicker.min.css',[],true) }}" rel="stylesheet" type="text/css" >
+<link href="{{ url('/css/bootstrap-select.min.css',[],true) }}" rel="stylesheet" type="text/css" >
+<link href="{{ url('/css/bootstrap-colorpicker.min.css',[],true) }}" rel="stylesheet" type="text/css" >
+<link href="{{ url('/css/style.css',[],true) }}" rel="stylesheet" type="text/css" >
