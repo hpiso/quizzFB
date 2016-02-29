27ec96948d9fee751906ae60d04a618f5374d775
Antoine Cusset <a.cusset@gmail.com>
2016-02-29 20:43:29 +0100

    changement du middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,7 +59,7 @@ $app->middleware([
     Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
     Illuminate\Session\Middleware\StartSession::class,
     Illuminate\View\Middleware\ShareErrorsFromSession::class,
-    App\Http\Middleware\HttpsProtocol::class,
+//    App\Http\Middleware\HttpsProtocol::class,
 //    Laravel\Lumen\Http\Middleware\VerifyCsrfToken::class,
 ]);
 
