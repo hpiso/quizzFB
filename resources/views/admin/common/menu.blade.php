c4c6599b0a1025a8d4e7b2bd1418f05fffb389c4
Antoine Cusset <a.cusset@gmail.com>
2016-02-29 22:46:06 +0100

    encore du secure

--- resources/views/admin/common/menu.blade.php
+++ resources/views/admin/common/menu.blade.php
@@ -1,20 +1,20 @@
 <li>
-    <a href="{{ route('front.index') }}"><i class="fa fa-external-link fa-fw"></i> Retour au quizz</a>
+    <a href="{{ url('/',[],true) }}"><i class="fa fa-external-link fa-fw"></i> Retour au quizz</a>
 </li>
 <li>
-    <a href="{{ route('dashboard.index') }}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+    <a href="{{ url('admin/dashboard',[],true) }}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
 </li>
 <li>
-    <a href="{{ route('quizz.index') }}"><i class="fa fa-gamepad fa-fw"></i> Quizz</a>
+    <a href="{{ url('admin/quizz',[],true) }}"><i class="fa fa-gamepad fa-fw"></i> Quizz</a>
 </li>
 <li>
-    <a href="{{ route('question.index') }}"><i class="fa fa-question fa-fw"></i> Questions</a>
+    <a href="{{ url('admin/question',[],true) }}"><i class="fa fa-question fa-fw"></i> Questions</a>
 </li>
 <li>
-    <a href="{{ route('theme.index') }}"><i class="fa fa-picture-o fa-fw"></i> Theme</a>
+    <a href="{{ url('admin/theme',[],true) }}"><i class="fa fa-picture-o fa-fw"></i> Theme</a>
 </li>
 <li>
-    <a href="{{ route('users.index') }}"><i class="fa fa-users"></i> Utilisateurs</a>
+    <a href="{{ url('admin/users') }}"><i class="fa fa-users"></i> Utilisateurs</a>
 </li>
 <li>
     <a href="{{ url('logout',[],true) }}"><i class="fa fa-sign-out fa-fw"></i> Se déconnecter</a>
